aad1c517ed707dbed0013b0a8bcbaa1c8b5b14cd
gabriel aleixo <aleixo.gabriel.aleixo@gmail.com>
2024-09-27 14:21:23 -0300

    Header 2

--- README.md
+++ README.md
@@ -7,4 +7,5 @@
     npm run dev
     npm install react-router-dom
     npm run build
+    npm install bootstrap@5.3.3
 ```
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,39 +3,58 @@ import { Link } from "react-router-dom";
 import styles from '../styles/NavBar.module.css';
 
 function NavBar() {
-    const [menuOpen, setMenuOpen] = useState(false);  // Controla se o menu está aberto ou fechado
+    // Usando useState para controlar se o menu está aberto ou fechado
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    // Função para alternar o estado do menu
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
     return (
-        <header className={styles.header}>
-            <div 
-                className={styles.menuIcon} 
-                onClick={() => setMenuOpen(!menuOpen)}  // Alterna o estado entre aberto e fechado
-            >
-                &#9776;  {/* Ícone de menu hamburguer */}
+        <>
+            {/* Botão para abrir o menu */}
+            <div className={styles.btnabrir} onClick={toggleMenu}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
+                </svg>
             </div>
-            <nav className={styles.navBar}>
-                <ul className={`${styles.navList} ${menuOpen ? styles.active : ''}`}>
-                    <li>
-                        <Link to="/" className={styles.navItem}>Inicio</Link>
-                    </li>
-                    <li>
-                        <Link to="/doacao" className={styles.navItem}>Doação</Link>
-                    </li>
-                    <li>
-                        <Link to="/estoque" className={styles.navItem}>Estoque</Link>
-                    </li>
-                    <li>
-                        <Link to="/sobre" className={styles.navItem}>Sobre</Link>
-                    </li>
-                    <li>
-                        <Link to="/solicitacao" className={styles.navItem}>Solicitação</Link>
-                    </li>
-                    <li>
-                        <Link to="/suporte" className={styles.navItem}>Suporte</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+
+            {/* Header com menu, aplicando classes dinamicamente */}
+            <header className={`${styles.header} ${menuOpen ? styles.abrMenu : ''}`}>
+                <div className={styles.btnfechar} onClick={toggleMenu}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
+                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                    </svg>
+                </div>
+
+                <nav className={styles.navBar}>
+                    <ul className={styles.navList}>
+                        <li>
+                            <Link to="/" className={styles.navItem}>Inicio</Link>
+                        </li>
+                        <li>
+                            <Link to="/doacao" className={styles.navItem}>Doação</Link>
+                        </li>
+                        <li>
+                            <Link to="/estoque" className={styles.navItem}>Estoque</Link>
+                        </li>
+                        <li>
+                            <Link to="/sobre" className={styles.navItem}>Sobre</Link>
+                        </li>
+                        <li>
+                            <Link to="/solicitacao" className={styles.navItem}>Solicitação</Link>
+                        </li>
+                        <li>
+                            <Link to="/suporte" className={styles.navItem}>Suporte</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+
+            {/* Background escuro que aparece quando o menu está aberto */}
+            {menuOpen && <div className={styles.escMenu} onClick={toggleMenu}></div>}
+        </>
     );
 }
 
