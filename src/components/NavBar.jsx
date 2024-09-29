8e6ba8ebd53f3417c5ad0ad87c1c262780f36b5e
gabriel aleixo <aleixo.gabriel.aleixo@gmail.com>
2024-09-29 11:12:07 -0300

    Mudança posição Header

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,8 +14,8 @@ function NavBar() {
     return (
         <>
             {/* Botão para abrir o menu */}
-            <div className={styles.btnabrir} onClick={toggleMenu}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+            <div className={styles.btnabrir}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16" onClick={toggleMenu}>
                     <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
                 </svg>
             </div>
